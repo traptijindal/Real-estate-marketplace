e8593d91ad95658791fc957745f7d4ac7ec70c02
traptijindal <traptijindal5678@gmail.com>
2023-11-14 17:29:08 +0530

    error resolve

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,15 @@
 import {useSelector} from 'react-redux'
+import {  useState} from 'react';
+
 
 export default function Profile() { 
-  const {currentUser} = useSelector((state) => state.user)
+
+  const {currentUser} = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) =>{
+    setFormData({...formData , [e.target.id] : e.target.value });
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-white text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -9,15 +17,22 @@ export default function Profile() {
 
       <img src={currentUser.avatar} alt="profile"
       className='rounded-full h-24 w-24 objec-cover cursor-pointer self-center mt-2'/>
-      <input type='text' placeholder='username'
+      <input 
+      type='text'
+       placeholder='username'
+       defaultValue = {currentUser.username}
       id='username'
-      className='border p-3 rounded-lg'/>
+      className='border p-3 rounded-lg'
+      onChange ={handleChange}/>
        <input type='email' placeholder='email'
       id='email'
-      className='border p-3 rounded-lg'/>
+      defaultValue={currentUser.email}
+      className='border p-3 rounded-lg'
+      onChange ={handleChange}/>
        <input type='password' placeholder='password'
       id='password'
-      className='border p-3 rounded-lg'/>
+      className='border p-3 rounded-lg'
+      onChange ={handleChange}/>
       <button 
       className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
          update
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch , useSelector} from 'react-redux';
-import { signInStart , signInSuccess,signInFailure } from '../redux/user/userSlice.js';
-import OAuth from '../components/OAuth.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const { loading , error} = useSelector((state) => state.user);
-  // const [error, setError] = useState(null);
-  // const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  const dispatch =useDispatch();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -20,7 +22,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      dispatch(signInStart());                               //setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -31,63 +33,49 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-          dispatch(signInFailure(data.message));                                     // setLoading(false) ; setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-                                      
-     
-      dispatch(signInSuccess(data));                                                  // setLoading(false);  // setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
       dispatch(signInFailure(error.message));
-      // setLoading(false);
-      // setError(error.message);
     }
   };
-
-
   return (
-    <div className=' p-3 max-w-lg mx-auto'>
-      <h1 className='text-white text-3xl text-center font-semibold my-7'>Signin</h1>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-white text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-      {/* <input
-          type='text'
-          placeholder='username'
-          className='border p-3 rounded-lg'
-          id='username'
-          onChange={handleChange}
-        /> */}
         <input
-        type="email" 
-        placeholder="email" 
-        id="email" 
-        className='border p-3 rounded-lg'
-        onChange={handleChange}
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
         />
-        <input type="password" 
-        placeholder="password" 
-        id="password"
-        className='border p-3 rounded-lg'
-        onChange={handleChange} 
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
         />
-         <button 
+
+        <button
           disabled={loading}
-          className='text-white bg-slate-700 p-3 rounded-lg  hover:opacity-95 disabled:opacity-80'>
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
           {loading ? 'Loading...' : 'Sign In'}
-         
-         
-         </button>
-         <OAuth/>
-         </form>
-         <div className='flex gap-2 mt-5'>
-         <span className='text-blue-700'>Don't have? create account </span>
-         <Link to={"/sign-up"}>
-         <p className='text-white'>Sign up</p>
-         </Link>
-        
-      
-       </div>
-      {error && <p className ='text-red-500 mt-5'>{error}</p>}
+        </button>
+        <OAuth/>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p className='text-white'>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
