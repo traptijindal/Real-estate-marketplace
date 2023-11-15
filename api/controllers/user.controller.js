5ec14cdd8d7682aaa96ada0dfe366a2002ef25f6
traptijindal <traptijindal5678@gmail.com>
2023-11-15 16:13:54 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -34,13 +34,29 @@ export const updateUser = async (req,res,next)  => {
     next(error)
    }
 };
+export const deleteUser = async(req, res , next) =>{
+if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account'))
+try{
+    await User.findByIdAndDelete(req.params.id)
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deletd');
+
+}catch(error){
+    next(error)
+}
+}
+
+
+
+
+
 
 
 
 // import bcryptjs from 'bcryptjs';
 // import User from '../models/user.model.js';
 // import { errorHandler } from '../utils/error.js';
-// import Listing from '../models/listing.model.js';
+// // import Listing from '../models/listing.model.js';
 
 // export const test = (req, res) => {
 //   res.json({
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,9 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
-  // deleteUserFailure,
-  // deleteUserStart,
-  // deleteUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
   // signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
@@ -94,22 +94,22 @@ export default function Profile() {
     }
   };
 
-  // const handleDeleteUser = async () => {
-  //   try {
-  //     dispatch(deleteUserStart());
-  //     const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-  //       method: 'DELETE',
-  //     });
-  //     const data = await res.json();
-  //     if (data.success === false) {
-  //       dispatch(deleteUserFailure(data.message));
-  //       return;
-  //     }
-  //     dispatch(deleteUserSuccess(data));
-  //   } catch (error) {
-  //     dispatch(deleteUserFailure(error.message));
-  //   }
-  // };
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
 
   // const handleSignOut = async () => {
   //   try {
@@ -190,11 +190,11 @@ export default function Profile() {
         </button>
     </form>
     <div className=" flex justify-between mt-5">
-      <span className='text-red-700 cursor-pointer'> Delete Account</span>
+      <span onClick={handleDeleteUser}className='text-red-700 cursor-pointer'> Delete Account</span>
       <span className='text-red-700 cursor-pointer'> Sign Out</span>
     </div>
 
-    <p className='text-red-700  mt-5' >{error? error: ''}</p>
+    {/* <p className='text-red-700  mt-5' >{error? error: ''}</p> */}
     <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully' :  ' '} </p>
 
     </div>
